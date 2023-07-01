c85f84d4cc407cc70dde657bd80fdfdd64259322
Siya021 <siyamthandamazokwana@gmail.com>
2023-07-01 14:35:14 +0200

    about edited

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,11 +3,12 @@
     <h1>About</h1>
     <div class="container">
       <div class="row">
-        <div class="col">
+        <div class="col" id="aboutIMG">
           <img src="https://i.postimg.cc/90kt1L4b/Ellipse-3.png" class="aboutImg">
         </div>
         <div class="col"><p class="description">I'm a Front-End Developer from Cape Town, South Africa
            I am Siyamthanda Mazokwana, 24 years of age and I am an Aspiring Front End Developer</p>
+           <button class="btn btn-outline-success" to="/contact">Contact Me</button>
           </div>
         </div>
       </div>
@@ -18,9 +19,10 @@
   margin-top: 20px;
   align-items: center;
 }
-.container{
-  border: solid green;
+#aboutIMG{
+  border-right: solid green;
 }
+
 .aboutImg{
   height: 550px;
 }
