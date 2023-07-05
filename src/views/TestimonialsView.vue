78e3a8c39b12e25b3566ece5f27d1dc7717569e1
Siya021 <siyamthandamazokwana@gmail.com>
2023-07-05 16:43:14 +0200

    testimonials in place

--- src/views/TestimonialsView.vue
+++ src/views/TestimonialsView.vue
@@ -1,15 +1,104 @@
 <template>
     <div>
         <h1>Testimonials</h1>
+        <div class="row">
+            <div class="col " v-for="item in testimonials" :key="item.id">
+                <div class="card">
+                  <div class="image-content">
+                    <span class="overlay"></span>
+    
+                    <div class="card-image">
+                      <img :src= item.image alt="" class="card-img">
+                    </div>
+                  </div>
+                  <div class="card-content">
+                    <h2 class="name">{{ item.name }}</h2>
+                    <p class="description">{{ item.message }}</p>
+                  </div>
+                </div>
+              </div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
+        computed: {
+    testimonials() {
+      return this.$store.state.testimonials;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("fetchTestimonials");
+  },
         
     }
 </script>
 
 <style scoped>
-
+.card{
+    width: 300px;
+    border-radius: 25px;
+    background-color:white;
+    scroll-snap-align: start;
+  }
+  .image-content,
+  .card-content{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px 14px;
+  }
+  .image-content{
+    position: inherit;
+    row-gap: 5px;
+    
+  }
+  .overlay{
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    background-color: red;
+    border-radius: 25px 25px 0 25px;
+  }
+  .overlay::before,
+  .overlay::after{
+    content: '';
+    position: relative;
+    right: 0;
+    bottom: -40px;
+    height: 40px;
+    width: 40px;
+    background-color: black;
+  }
+  .overlay::after{
+    border-radius: 0 25px 0 0;
+    background-color: red;
+  }
+   .card-image{
+    position: relative;
+    height: 150px;
+    width: 150px;
+    border-radius: 75%;
+    background: red;
+    padding: 4px;
+   } 
+  .card-image .card-img{
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    border-radius: 50%;
+    border: 3px solid black;
+  }
+  .name{
+    font-size: 18px;
+    font-weight: 500;
+  }
+  .description{
+    font-size: 15px;
+    text-align: center;
+  }
+  
 </style>
