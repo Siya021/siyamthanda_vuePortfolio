4f4e9c9c4f2f9d3b0caa5e2dd063c27acd0954b8
Siya021 <siyamthandamazokwana@gmail.com>
2023-07-05 16:15:33 +0200

    skills in place

---
+++ src/components/SkillsComp.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <button class="btn btn-outline-success">HTML</button>
+        <button class="btn btn-outline-success">CSS</button>
+        <button class="btn btn-outline-success">JS</button>
+        <button class="btn btn-outline-success">Bootstrap</button>
+        <button class="btn btn-outline-success">VueJS</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.container{
+    justify-content: space-between;
+}
+.btn{
+    font-size: 1rem !important;
+    width: 100px;
+    text-decoration: none;
+    color: orange;
+    height: 100px;
+    border-radius: 50px;
+    padding: inherit;
+    margin: 5px;
+}
+</style>
